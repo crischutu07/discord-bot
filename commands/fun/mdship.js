6ed3b51931ba0ce57b32c932f26f6709924472d3
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-10-02 23:59:25 +0700

    hope this work again

--- commands/fun/mdship.js
+++ commands/fun/mdship.js
@@ -98,7 +98,7 @@ module.exports = {
       }
     } else {
       if (a.get(characters) === undefined)
-        interaction.reply({ content: "Please enter a valid Murder Drones characters.\n-# Tip: Use names from **[this](<\https://murder-drones.fandom.com/wiki/Category:Characters>)** pages", ephemeral: true })
+        return interaction.reply({ content: "Please enter a valid Murder Drones characters.\n-# Tip: Use names from **[this](<\https://murder-drones.fandom.com/wiki/Category:Characters>)** pages", ephemeral: true })
       c1 = {
         name: characters,
         url: a.get(characters)

3754f323f67a63c3da07bd1603dbb9274e7ddde1
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-10-02 10:40:10 +0700

    wip

---
+++ commands/fun/petpet.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+module.exports = {
+  disabled: false,
+  data: {
+    name: "petpet",
+    description: "Pet a user by generating a patting gif.",
+    contexts: [0,1,2],
+    integration_types: [0,1],
+    dm_permissions: true,
+    options: [
+      {
+        name: "user",
+        description: "User you want to pet.",
+        required: true,
+        type: 6,
+      }
+    ]
+  },
+  /**
+   *
+   * @param {SlashCommandBuilder} interactions
+   *
+   */
+  async execute(interactions) {
+    const user = interactions.options.getUser('user');
+    interactions.reply({ content: `${user}`, ephemeral: true })
+  }
+}

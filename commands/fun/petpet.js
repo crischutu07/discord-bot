aa9d64f05f05df7c58bb83585f1691fc7f359c53
crischutu07 <sondolavnxbox.omlet@gmail.com>
2024-10-02 11:01:09 +0700

    hope this work

--- commands/fun/petpet.js
+++ commands/fun/petpet.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, PermissionsBitField, User, UserFlagsBitField } = require('discord.js');
+const petPetGif = require('pet-pet-gif');
+
 module.exports = {
   disabled: false,
   data: {
@@ -21,8 +23,16 @@ module.exports = {
    * @param {SlashCommandBuilder} interactions
    *
    */
-  async execute(interactions) {
+  async execute(interactions, petpet) {
     const user = interactions.options.getUser('user');
-    interactions.reply({ content: `${user.avatarURL()}`, ephemeral: true })
+    let animatedGif = await petpet(user.avatarURL(), {
+      // The width (or height) of the generated gif
+      resolution: 128,
+      // Delay between each frame in milliseconds. Defaults to 20.
+      delay: 20,
+      // Other values could be the string "rgba(123, 233, 0, 0.5)". Defaults to null - i.e. transparent.
+      backgroundColor: null
+    })
+    interactions.reply({ files: [animatedGif], ephemeral: true })
   }
 }
